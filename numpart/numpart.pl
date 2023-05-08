55f028942f3119c742c44a8dfe2bac22b5c1b1b2
uptheshipcreek <northenore@gmail.com>
2023-05-08 19:21:39 +0300

    I don't think I am wasting any more time in this

--- numpart/numpart.pl
+++ numpart/numpart.pl
@@ -1,37 +1,30 @@
+:- use_module(library(ic)).
 :- use_module(library(ic_global)).
-:- use_module(library(clpfd)).
-
 
 squares_sum(List,SquaredSum):-squares_sum(List,List,0,SquaredSum).
 squares_sum(_,[],SquaredSum,SquaredSum).
 squares_sum(List,[N|RemainingList],SoFarSum,SquaredSum):-
-    NewSum is SoFarSum + N*N,
+    NewSum #= SoFarSum + N*N,
     squares_sum(List,RemainingList,NewSum,SquaredSum).
-
 
 check(List,N):-
     %calculate the n(n+1)/4 and check
-    SumsoNums is (N*(N+1))/4,
+    SumsoNums #= (N*(N+1))/4,
     ordered_sum(List,S1),
-    SumsoNums =:= S1,
+    SumsoNums #= S1,
     %calculate the n(n+1)(2*n+1)/12 and check
-    SumsoNumSquares is (N*(N+1)*((2*N)+1))/12,
+    SumsoNumSquares #= (N*(N+1)*((2*N)+1))/12,
     squares_sum(List,S2),
-    SumsoNumSquares =:= S2.
-
-disjoint(_,[]).
-disjoint(L1,[E|L2]):-
-    \+member(E,L1),
-    disjoint(L1,L2).
+    SumsoNumSquares #= S2.
 
 numpart(N, [1|L1], L2):-
     N1 #= (N/2)-1,N2 #= N/2,
     length(L1,N1),length(L2,N2),
-    L1 ins 2..N,L2 ins 2..N,
-    append(L1,L2,L),
-    all_different(L),
-    label(L1),check([1|L1],N),
-    sorted(L2,L2),disjoint(L1,L2),
-    label(L2).
+    L1 :: 2..N,L2 :: 2..N,
+    append([1|L1],L2,L),
+    ic:alldifferent(L),
+    check([1|L1],N),labeling(L1),
+    sorted(L2,L2),%disjoint(L1,L2),
+    labeling(L2).
 
 
