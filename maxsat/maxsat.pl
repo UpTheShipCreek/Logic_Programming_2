9867fe74046bc908815969ad31632add5449790a
uptheshipcreek <northenore@gmail.com>
2023-05-11 17:08:46 +0300

    It's trash but I ma pushing it anyways, should have fucking payed attention to the lectures but I am so fucking bored BROOOOOOO

--- maxsat/maxsat.pl
+++ maxsat/maxsat.pl
@@ -1,13 +1,21 @@
 :- use_module(library(ic)).
-:- use_module(library(ic_global)).
 
 %index of an element in a list starting from 1
-index(_, [], _):-fail.
-index(Element,[Element|_],1).
-index(Element,[_|List],Index):-
-  index(Element,List,Index0),succ(Index0,Index).
+%index(_, [], _):-fail.
+%index(Element,[Element|_],1).
+%index(Element,[_|List],Index):-
+%  index(Element,List,Index0),succ(Index0,Index).
+
+%element(?Index, +Values, ?Element)
+    %Element is the Index'th element of the collection Values.
 
 head([H|_],H).
+rhead([_|T],H):-reverse(T,R),head(R,H).
+
+print_list([]).
+print_list([H|T]) :-
+   write(H), nl,
+   print_list(T).
 
 %maxsat(NV, NC, D, F, S, M)
 %   NV -> Number of Variables
@@ -19,28 +27,31 @@ head([H|_],H).
 
 maxsat(NV,NC,D,F,S,M):-
     length(S,NV),
-    create_formula(NV,NC,D,F),
-    S :: 0..1,
-    formula_eval(F,S,M),
-    find_max(NC,M),
+    S #:: 0..1,
+    N #:: 1..NC,
+    M #:: 1..NC,
+    %create_formula(NV,NC,D,F),
+    %findall(N,formula_eval(F,S,N),List),
+    %member(List,Member),
+    %get_max(Member,N),
+    formula_eval(F,S,N),
+    %max_check(F,S,N),
+    get_max(N,M),
     labeling(S).
-
-find_max(NC,M):-
-    M #= NC;
-    NNC is NC-1, find_max(NNC,M).
 
 formula_eval(F,S,NumOfTrueClauses):-
     formula_eval(F,F,S,0,NumOfTrueClauses),!.
 formula_eval(_,[],_,NumOfTrueClauses,NumOfTrueClauses):-!.
 formula_eval(F,[Clause|RestClauses],S,SoFarNum,NumOfTrueClauses):-
     clause_eval(Clause,S,Result),
-    (Result #> 0, Add #= 1; Add #=0),
+    %(Result #> 0, ac_eq(NewSoFar,SoFarNum,1); ac_eq(NewSoFar,SoFarNum,0)),   
+    ((Result #> 0, Add #= 1); Add #= 0),   
     NewSoFar #= SoFarNum + Add,
     formula_eval(F,RestClauses,S,NewSoFar,NumOfTrueClauses).
 
 clause_eval(Clause,S,Result):- 
-    clause_eval(Clause,Clause,S,0,Result).
+    clause_eval(Clause,Clause,S,0,Result),!.
 clause_eval(_,[],_,Result,Result).
 clause_eval(Clause,[Var|RestCl],S,SoFarRes,Result):- %Clause should be a list of form [X1,-X2,...]
-    (abs(Var,Var),index(Ele,S,Var),NewRes #= (SoFarRes + Ele),clause_eval(Clause,RestCl,S,NewRes,Result);
-    \+abs(Var,Var),abs(Var,Pos),index(Ele,S,Pos),NewRes #= (SoFarRes + (1 - Ele))),clause_eval(Clause,RestCl,S,NewRes,Result).
+    (abs(Var,Var),element(Var,S,Ele),NewRes #= (SoFarRes + Ele),clause_eval(Clause,RestCl,S,NewRes,Result);
+    \+abs(Var,Var),abs(Var,Pos),element(Pos,S,Ele),NewRes #= (SoFarRes + (1 - Ele))),clause_eval(Clause,RestCl,S,NewRes,Result).
